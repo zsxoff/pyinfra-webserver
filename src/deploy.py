5d5fb3965d320f890525e174062651fc20912ee6
Konstantin Dobratulin <zsxoff@gmail.com>
2026-03-30 00:26:30 +0300

    feat: compact commands for faster running

--- src/deploy.py
+++ src/deploy.py
@@ -49,7 +49,11 @@ apt.packages(
         "apt-listchanges",
         "ca-certificates",
         "debsums",
+        "fail2ban",
         "locales",
+        "logrotate",
+        "openssh-server",
+        "ufw",
         "unattended-upgrades",
     ],
     latest=True,
@@ -62,50 +66,18 @@ apt.packages(
 
 # UFW
 
-apt.packages(
-    name="APT - Install UFW",
-    packages=["ufw"],
-    latest=True,
-    no_recommends=True,
-    present=True,
-    update=True,
-    cache_time=APT_CACHE_TIME,
-    _sudo=True,
-)
-
 server.shell(
-    name="UFW - Allow SSH",
-    commands=["ufw allow ssh"],
-    _sudo=True,
-)
-
-server.shell(
-    name="UFW - Allow 443/TCP",
-    commands=["ufw allow 443/tcp"],
-    _sudo=True,
-)
-
-server.shell(
-    name="UFW - Allow 443/UDP",
-    commands=["ufw allow 443/udp"],
-    _sudo=True,
-)
-
-server.shell(
-    name="UFW - Default deny incoming",
-    commands=["ufw default deny incoming"],
-    _sudo=True,
-)
-
-server.shell(
-    name="UFW - Default allow outgoing",
-    commands=["ufw default allow outgoing"],
-    _sudo=True,
-)
-
-server.shell(
-    name="UFW - Force enable",
-    commands=["ufw --force enable"],
+    name="UFW - Configure firewall rules in one shot",
+    commands=[
+        "ufw reset",
+        "ufw allow ssh",
+        "ufw allow 443/tcp",
+        "ufw allow 443/udp",
+        "ufw default deny incoming",
+        "ufw default allow outgoing",
+        "ufw logging on",
+        "ufw --force enable",
+    ],
     _sudo=True,
 )
 
@@ -119,17 +91,6 @@ systemd.service(
 
 # Fail2ban
 
-apt.packages(
-    name="APT - Install Fail2ban",
-    packages=["fail2ban"],
-    latest=True,
-    no_recommends=True,
-    present=True,
-    update=True,
-    cache_time=APT_CACHE_TIME,
-    _sudo=True,
-)
-
 systemd.service(
     name="Systemd - Enable Fail2ban",
     service="fail2ban.service",
@@ -141,17 +102,6 @@ systemd.service(
 # TODO(zsxoff): Copy /etc/fail2ban/jail.conf to jail.local.
 
 # Logrotate
-
-apt.packages(
-    name="APT - Install Logrotate",
-    packages=["logrotate"],
-    latest=True,
-    no_recommends=True,
-    present=True,
-    update=True,
-    cache_time=APT_CACHE_TIME,
-    _sudo=True,
-)
 
 systemd.service(
     name="Systemd - Enable Logrotate",
